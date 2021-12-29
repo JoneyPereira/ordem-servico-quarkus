986753e42cfb5666c910b3119f47e0244b4a61e7
JoneyPereira <joneysousa@yahoo.com.br>
2021-12-29 20:17:03 -0300

    excluir arquivos

--- ordem-de-servico/src/main/resources/import.sql
+++
@@ -1,5 +0,0 @@
-insert into OrdemServico(id, descricao, data, categoria ) values(nestval('hibernate_sequence'), 'Realizar troca de impressora.', '2021,29,12', 'URGENTE');
-insert into OrdemServico(id, descricao, data, categoria ) values(nestval('hibernate_sequence'), 'Realizar troca de cpu.', '2021,29,12', 'NAO URGENTE');
-insert into OrdemServico(id, descricao, data, categoria ) values(nestval('hibernate_sequence'), 'Realizar troca de roteador.', '2021,29,12', 'OUTRAS');
-insert into OrdemServico(id, descricao, data, categoria ) values(nestval('hibernate_sequence'), 'Realizar troca de cabo de rede.', '2021,29,12', 'URGENTE');
-insert into OrdemServico(id, descricao, data, categoria ) values(nestval('hibernate_sequence'), 'Realizar troca de impressora.', '2021,29,12', 'OUTRAS');
